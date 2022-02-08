aa3d3b5b38f8e1a4750506cb85ffb038d1d1cdb5
wheeleral <awheeler2@wpi.edu>
2022-02-07 20:59:18 -0500

    Why hello there, we added a constants folder which consists of most of the data from SystemID. We also converted the velocity to voltage. -(0_0)-

--- competition/src/main/java/frc/robot/Robot.java
+++ competition/src/main/java/frc/robot/Robot.java
@@ -4,13 +4,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
 import frc.robot.util.NtHelper;
 import frc.robot.util.DriveHelper;
+import frc.robot.constants.constants;
+import edu.wpi.first.wpilibj.RobotController;
+
 
 public class Robot extends TimedRobot {
-
+  public SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(constants.Ks, constants.Kv);
   @Override
   public void robotInit() {
     Devices.init();
@@ -38,11 +42,20 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     double turnRate = DriveHelper.applyDeadband(-Devices.controller.getLeftX());
     double ySpeed = DriveHelper.applyDeadband(-Devices.controller.getLeftY());
+
     double[] arcadeSpeeds = DriveHelper.getArcadeSpeeds(ySpeed *.3, -turnRate *.3, false);
-    double leftSpeed = arcadeSpeeds[0];
-    double rightSpeed = arcadeSpeeds[1];
-    Devices.leftMotor.setPercent(leftSpeed);
-    Devices.rightMotor.setPercent(rightSpeed);
+
+    double leftSpeed = arcadeSpeeds[0] * edu.wpi.first.math.util.Units.feetToMeters(constants.maxSpeedo);
+    double rightSpeed = arcadeSpeeds[1] * edu.wpi.first.math.util.Units.feetToMeters(constants.maxSpeedo);
+
+    double leftVoltage = feedforward.calculate(leftSpeed);
+    double rightVoltage = feedforward.calculate(rightSpeed);
+
+    double leftPercent = leftVoltage * RobotController.getBatteryVoltage();
+    double rightPercent = rightVoltage * RobotController.getBatteryVoltage();
+
+    Devices.leftMotor.setPercent(leftPercent);
+    Devices.rightMotor.setPercent(rightPercent);
   }
 
 }
