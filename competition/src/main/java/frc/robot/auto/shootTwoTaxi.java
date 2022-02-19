1a1c4e109cbca26d71227e4b88b903d52da77695
wheeleral <awheeler2@wpi.edu>
2022-02-19 15:03:17 -0500

    ugh

---
+++ competition/src/main/java/frc/robot/auto/shootTwoTaxi.java
@@ -0,0 +1,148 @@
+package frc.robot.auto;
+import frc.robot.util.stateMachine.InitState;
+import frc.robot.util.stateMachine.RunState;
+import frc.robot.util.stateMachine.StateMachine;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.util.NtHelper;
+import frc.robot.util.DriveHelper;
+import frc.robot.Devices;
+import frc.robot.constants.constants;
+import frc.robot.subsystem.Drivetrain;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import frc.robot.util.Targeting;
+import frc.robot.util.TrajectoryFollower;
+
+import frc.robot.util.TrajectoryFollower;
+
+import java.util.List;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import frc.robot.subsystem.Intake;
+
+/* Move towards cargo in straight line
+ * Intake cargo
+ * Aim and shoot both cargos at upper hub (testing necessary)
+ * Move backwards to taxi
+ */ 
+
+public class shootTwoTaxi extends StateMachine{
+
+    private Intake intake = new Intake ();
+    Trajectory CargoAdvance = TrajectoryGenerator.generateTrajectory(
+            //the line is going along the x axis
+            new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+            List.of(),
+            new Pose2d(Units.feetToMeters(10), 0, Rotation2d.fromDegrees(0)),
+            new TrajectoryConfig(Units.feetToMeters(constants.maxSpeedo), Units.feetToMeters(constants.maxAccel))
+        );
+    Trajectory ShooterAdvance = TrajectoryGenerator.generateTrajectory(
+        //the line is going along the x axis
+        new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+        List.of(),
+        new Pose2d(Units.feetToMeters(20), 0, Rotation2d.fromDegrees(0)),
+        new TrajectoryConfig(Units.feetToMeters(constants.maxSpeedo), Units.feetToMeters(constants.maxAccel))
+    );
+    Trajectory TaxiBack = TrajectoryGenerator.generateTrajectory(
+        //the line is going along the x axis
+        new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+        List.of(),
+        new Pose2d(Units.feetToMeters(30), 0, Rotation2d.fromDegrees(0)),
+        new TrajectoryConfig(Units.feetToMeters(constants.maxSpeedo), Units.feetToMeters(constants.maxAccel))
+    );
+    private TrajectoryFollower follower = new TrajectoryFollower();
+    private Timer timer = new Timer();
+
+
+    public shootTwoTaxi() {
+        super("CargoAdvance");
+        follower.addTrajectory("CargoAdvance", CargoAdvance);
+        follower.addTrajectory("ShooterAdvance", ShooterAdvance);
+        follower.addTrajectory("TaxiBack", TaxiBack);
+    }
+
+    @InitState(name = "CargoAdvance")
+    public void CargoAdvanceInit (){
+        follower.setTrajectory("CargoAdvance");
+        follower.resetPosition();
+    }
+
+    @RunState(name = "CargoAdvance")
+    public void CargoAdvance(){
+        follower.follow();
+        if (follower.isDone()){
+            this.setState("Intake");
+        }
+        
+    }
+
+    @InitState(name = "Intake")
+    public void IntakeInit (){
+        intake.intakeDown();
+        timer.reset ();
+        timer.start ();
+    }
+
+    @RunState(name = "Intake")
+    public void Intake (){
+        if (timer.get() > 2){
+            this.setState("ShooterAdvance");
+        }
+        //Seconds; subject to change
+
+    }
+
+    @InitState(name = "ShooterAdvance")
+    public void ShooterAdvanceInit (){
+        follower.setTrajectory("ShooterAdvance");
+        follower.resetPosition();
+    }
+
+    @RunState(name = "ShooterAdvance")
+    public void ShooterAdvance (){
+        follower.follow ();
+        if (follower.isDone()){
+            this.setState ("ShootTwo");
+        }
+
+    }
+
+    @InitState(name = "ShootTwo")
+    public void ShootTwoInit (){
+        timer.reset();
+        timer.start();
+    //Shooter initialization
+    }
+
+    @RunState(name = "ShootTwo")
+    public void ShootTwo (){
+        //Shoot both cargos
+        if (timer.get() > 4){
+            this.setState ("TaxiBack");
+        }
+        //Seconds subject to change upon testing
+    
+    }
+
+    @InitState(name = "TaxiBack")
+    public void TaxiBackInit (){
+        follower.setTrajectory ("CargoAdvance");
+        follower.resetPosition();
+    }
+
+    @RunState(name = "TaxiBack")
+    public void TaxiBack (){
+        follower.follow ();
+    }
+
+
+}
---
+++ competition/src/main/java/frc/robot/util/TrajectoryFollower.java
@@ -0,0 +1,86 @@
+package frc.robot.util;
+
+import frc.robot.util.stateMachine.InitState;
+import frc.robot.util.stateMachine.RunState;
+import frc.robot.util.stateMachine.StateMachine;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.util.NtHelper;
+import frc.robot.util.DriveHelper;
+import frc.robot.Devices;
+import frc.robot.constants.constants;
+import frc.robot.subsystem.Drivetrain;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import frc.robot.util.Targeting;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import frc.robot.subsystem.Intake;
+
+public class TrajectoryFollower {
+    public Timer timer = new Timer ();
+    public Trajectory trajectory;
+    private final RamseteController m_ramseteController = new RamseteController();
+    private Drivetrain drivetrain = new Drivetrain(
+      constants.trackWidth, 
+      constants.Ks, 
+      constants.Kv, 
+      Devices.gyro.getRotation()
+    );
+    private Field2d m_field;
+
+    private HashMap<String, Trajectory> trajectoryMap = new HashMap<String, Trajectory>();
+
+    public TrajectoryFollower (){
+
+    }
+
+    public void addTrajectory (String name, Trajectory trajectoryName){
+        trajectoryMap.put(name, trajectoryName);
+    }
+
+    public void setTrajectory (String name){
+        trajectory = trajectoryMap.get (name);
+
+    }
+
+    public void follow (){
+        var currTime = timer.get();
+        var desiredPose = trajectory.sample(currTime);
+
+        drivetrain.updateOdometry(Devices.gyro.getRotation(), Devices.leftMotor.getDistance(), Devices.rightMotor.getDistance());
+        m_field.setRobotPose(drivetrain.getPose());
+        ChassisSpeeds refChassisSpeeds = m_ramseteController.calculate(drivetrain.getPose(), desiredPose);
+        double[] motorValues = drivetrain.getMotorValues(refChassisSpeeds);
+        Devices.leftMotor.setPercent(motorValues[0]);
+        Devices.rightMotor.setPercent(motorValues[1]);
+
+    }
+
+    public void resetPosition (){
+        timer.start ();
+        Devices.leftMotor.resetEncoder(0);
+        Devices.rightMotor.resetEncoder(0);
+        Devices.gyro.reset();
+        drivetrain.odometryReset(new Pose2d(0,0,Rotation2d.fromDegrees(0)), Devices.gyro.getRotation());
+        m_field.setRobotPose(drivetrain.getPose());
+    }
+
+    public boolean isDone (){
+        if (timer.get() > trajectory.getTotalTimeSeconds()){
+            return true;
+        }
+        return false;
+    }
+    
+}
