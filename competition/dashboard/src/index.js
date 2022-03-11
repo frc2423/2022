8965c2f37d8d3a08945abfb6e7365bd132b5122e
Amory Galili <amorygalili@gmail.com>
2022-03-11 13:25:39 -0500

    Updated dashboard svg

--- competition/dashboard/src/index.js
+++ competition/dashboard/src/index.js
@@ -2,31 +2,64 @@
 import { SVG, G } from "@svgdotjs/svg.js";
 import imagePath from "./oie_transparent.png";
 import intakeTopArm from './images/intake_top_arm.png';
+import intakeSidePlate from "./images/intake_side_plate.png";
 
 const draw = SVG().addTo("body").size(500, 700);
-const group = draw.group().attr({
-  "transform-origin": "5 100",
-  transform: "rotate(40)"
-});
-const redRect = draw.rect(10, 100).attr({ fill: "#f00" });
-const blueRect = draw.rect(100, 10).attr({
-  fill: "#00f",
-  "transform-origin": "5 5",
-  transform: "rotate(-25)"
-});
 
-group.add(redRect);
-group.add(blueRect);
+// const svgConfig = {
+//   name: 'driveBase',
+//   type: 'group',
+//   children: [
+//     {
+//       name: 'plateOne',
+//       type: 'image',
+//       src: 'src/images/intake_motor_mount.png',
+//       size: { x: 51, y: 100 },
+//       transform: "translate(264 217)"
+//     },
+//     {
+//       name: 'wheelOne',
+//       type: 'image',
+//       src: 'src/images/wheel.png',
+//       size: { x: 233, y: 245 },
+//       transformOrigin: 'center',
+//       transform: "translate(233, 245)"
+//     },
+//   ]
+// };
 
-draw.image(imagePath).size(116, 75).translate(0, 100);
+
+
+// const wheelOne = draw
+//   .image("src/images/wheel.png")
+//   .size(67, 67)
+//   .translate(233, 245)
+//   .attr({
+//     "transform-origin": "center"
+//   });
+
+
 const upArm = draw
   .image(intakeTopArm)
   .size(120, 100)
   // .translate(189.5, 171.5)
   .attr({
-    "transform-origin": "120 85",
-    transform: "translate(189.5, 171.5) rotate(25)"
+    "transform-origin": "112 82",
+    transform: "translate(189.5, 171.5) rotate(0)"
   });
+
+  const intakeThing = draw 
+    .image(intakeSidePlate)
+    .size(100, 75)
+    .attr({
+      "transform-origin": "92 20",
+      transform: "translate(110, 169) rotate(0)"
+    });
+
+
+
+  // const downArm = draw
+  //   .image()
 
 const wheelOne = draw
   .image("src/images/wheel.png")
@@ -52,14 +85,20 @@ const wheelTwo = draw
 const base = draw
   .image("src/images/robot_base.png")
   .size(300, 200)
-  .translate(200, 100);
+  .translate(200, 100)
 
 const plateOne = draw
   .image("src/images/intake_motor_mount.png")
   .size(51, 100)
   .translate(264, 217);
 
+
+
+
 const robot = draw.group();
+const intake = draw.group();
+
+
 const driveBase = draw.group();
 
 driveBase.add(plateOne);
@@ -67,6 +106,9 @@ driveBase.add(wheelOne);
 driveBase.add(wheelTwo);
 driveBase.add(wheelThree);
 driveBase.add(base);
+
+intake.add(upArm);
+
 driveBase.attr({
   //transform: "rotate(20) translate(150, 10) scale(1)"
 });
