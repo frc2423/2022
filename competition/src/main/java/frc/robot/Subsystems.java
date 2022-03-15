7fadf2676821aa4cf82913c0cb7bad36d1fa53fa
Emily Charles <emily.w.charles@gmail.com>
2022-03-14 21:02:55 -0400

    move subsystem creation into init function because they depend on devices

--- competition/src/main/java/frc/robot/Subsystems.java
+++ competition/src/main/java/frc/robot/Subsystems.java
@@ -7,18 +7,24 @@ import frc.robot.subsystem.*;
  * Static accessor for all robot subsystems.
  */
 public class Subsystems {
-    public static Climber climber = new Climber();
-    public static Drivetrain drivetrain = new Drivetrain(
-        constants.trackWidth, 
-        constants.Ks, 
-        constants.Kv, 
-        Devices.gyro.getRotation()
-    );
-    public static Intake intake = new Intake();
-    public static Shooter shooter = new Shooter();
-    public static TrajectoryFollower follower = new TrajectoryFollower();
+    public static Climber climber;
+    public static Drivetrain drivetrain;
+    public static Intake intake;
+    public static Shooter shooter;
+    public static TrajectoryFollower follower;
 
     static void init() {
+        climber = new Climber();
+        drivetrain = new Drivetrain(
+            constants.trackWidth, 
+            constants.Ks, 
+            constants.Kv, 
+            Devices.gyro.getRotation()
+        );
+        intake = new Intake();
+        shooter = new Shooter();
+        follower = new TrajectoryFollower();
+
         intake.zero();
         intake.stop();
     }
