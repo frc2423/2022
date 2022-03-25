ad1be6dfe14199ead12d31ffac8fc68d0349b884
Amory Galili <amorygalili@gmail.com>
2022-03-25 19:52:24 -0400

    wip

--- competition/src/main/java/frc/robot/subsystem/Shooter.java
+++ competition/src/main/java/frc/robot/subsystem/Shooter.java
@@ -23,7 +23,7 @@ public class Shooter extends StateMachine{
 
     private double beltSpeed = -0.2;
     private double kickerSpeed = -0.3;
-    private double shooterSpeed = -60;
+    private double shooterSpeed = -60; // -42 for low
 
     private double revDuration = 1;
     private SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(0.10397, 0.12786, 0.0085994);
