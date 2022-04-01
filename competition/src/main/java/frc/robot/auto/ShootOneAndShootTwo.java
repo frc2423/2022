13546e85123163318faf8781d9e1be02acb67f43
Amory Galili <amorygalili@gmail.com>
2022-03-31 20:47:35 -0400

    Hello, we don't want to lose our stuff again so here D: bye

--- competition/src/main/java/frc/robot/auto/ShootOneAndShootTwo.java
+++ competition/src/main/java/frc/robot/auto/ShootOneAndShootTwo.java
@@ -1,77 +1,58 @@
 package frc.robot.auto;
 
 import frc.robot.util.stateMachine.StateMachine;
-import frc.robot.util.stateMachine.InitState;
-import frc.robot.util.stateMachine.RunState;
+import frc.robot.util.stateMachine.State;
+import frc.robot.util.stateMachine.StateContext;
 import frc.robot.Subsystems;
 
-import edu.wpi.first.wpilibj.Timer;
-
-public class ShootOneAndShootTwo extends StateMachine{
-    private Timer timer = new Timer();
+public class ShootOneAndShootTwo extends StateMachine {
 
     public ShootOneAndShootTwo() {
         super("FirstShot");
-      }
-
-    @InitState(name = "FirstShot")
-    public void firstShotInit(){
-        Subsystems.follower.setTrajectory("BottomTarmacToCargosToHub");
-        timer.reset();
-        timer.start();
-        //TODO: Implement shooter code following general shooter implementation
     }
 
-    @RunState(name = "FirstShot")
-    public void firstShotRun(){
-        //shooter.shootOne();
-        if (timer.get() > 4){
-            timer.stop();
-            setState ("CargoAdvance");
+    @State(name = "FirstShot")
+    public void firstShot(StateContext ctx) {
+        if (ctx.isInit()) {
+            Subsystems.follower.setTrajectory("BottomTarmacToCargosToHub");
         }
-        //Seconds subject to change upon testing
+        // shooter.shootOne();
+        if (ctx.getTime() > 4) {
+            setState("CargoAdvance");
+        }
+        // Seconds subject to change upon testing
     }
 
-    @InitState(name = "CargoAdvance")
-    public void cargoAdvanceInit(){
-        Subsystems.follower.startFollowing();
-        Subsystems.intake.goDown();
-    }
-
-    @RunState(name = "CargoAdvance")
-    public void cargoAdvanceRun(){
+    @State(name = "CargoAdvance")
+    public void cargoAdvance(StateContext ctx) {
+        if (ctx.isInit()) {
+            Subsystems.follower.startFollowing();
+            Subsystems.intake.goDown();
+        }
         Subsystems.follower.follow();
         if (Subsystems.follower.isDone()) {
             setState("ShootTwo");
         }
     }
 
-    @InitState(name = "ShootTwo")
-    public void shootTwoInit(){
-        timer.reset();
-        timer.start();
-        Subsystems.intake.goUp();
-    }
-
-    @RunState(name = "ShootTwo")
-    public void shootTwoRun(){
-        //shooter.shootTwo();
-        if (timer.get() > 4){
-            timer.stop();
-            setState ("TaxiBack");
+    @State(name = "ShootTwo")
+    public void shootTwo(StateContext ctx) {
+        if (ctx.isInit()) {
+            Subsystems.intake.goUp();
         }
-        //Seconds subject to change upon testing
+        // shooter.shootTwo();
+        if (ctx.getTime() > 4) {
+            setState("TaxiBack");
+        }
+        // Seconds subject to change upon testing
     }
 
-    @InitState (name = "TaxiBack")
-    public void taxiBackInit(){
-        Subsystems.follower.setTrajectory("Taxi4");
-        Subsystems.follower.startFollowing();
-        timer.stop();
-    }
-
-    @RunState (name = "TaxiBack")
-    public void taxiBackRun (){
-        Subsystems.follower.follow ();
+    @State(name = "TaxiBack")
+    public void taxiBack(StateContext ctx) {
+        if (ctx.isInit()) {
+            Subsystems.follower.setTrajectory("Taxi4");
+            Subsystems.follower.startFollowing();
+        }
+        Subsystems.follower.follow();
     }
 }
